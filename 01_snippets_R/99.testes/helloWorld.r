418d674682b47e279d3709910f6b2e9653966fb4
Marcelo Bracali <37077877+mbracali@users.noreply.github.com>
2024-03-05 19:06:13 -0300

    repo folder update

---
+++ 01_snippets_R/99.testes/helloWorld.r
@@ -0,0 +1,30 @@
+# Aula ministrada em: 24/04/2019
+# Criacao de vetores para o exercico da aula de R
+
+vetLogico <- c(T,T,F,F)
+vetInt <- c(1L,2L,3L,4L)
+vetNum <- c(1.0,2.0,3.0,4.0)
+vetText <- c("1","2","3","4")
+vetIma <- c(1i,2i,3i,4i)
+
+
+# Cria uma lista com 5 elementos, utilizando os
+# vetores criados anteriormente
+
+lista <- list(vetLogico, vetInt, vetNum, vetText, vetIma)
+
+
+# Cria matrix 4 x 4 com 16 numeros pares
+
+matrix = matrix(c(2L,4L,6L,8L,10L,12L,14L,16L,18L,20L,22L,24L,26L,28L,30L,32L), nrow = 4)
+
+
+# Cria dataFrame com os vetores criados anteriormente
+
+df = data.frame("Logicos" = vetLogico, 
+                "Inteiros" = vetInt,
+                "Numericos" = vetNum,
+                "Texto" = vetText,
+                "Imaginarios" = vetIma)
+
+row.names(df) <- c("L1","L2","L3","L4")
---
+++ 03_local_llm_testing/01_models/00_readme.txt
@@ -0,0 +1,8 @@
+Guarde os seus modelos nessa pasta.
+
+Download dos modelos pode ser feito em:
+
+* LLAMA2 CHAT 7b: https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/blob/main/llama-2-7b-chat.Q5_K_M.gguf
+* LLAMA2 CHAT 13b: https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/blob/main/llama-2-13b-chat.Q5_K_S.gguf
+* Vicuna 13b Q5: https://huggingface.co/TheBloke/vicuna-13B-v1.5-16K-GGUF/blob/main/vicuna-13b-v1.5-16k.Q5_K_S.gguf
+* Vicuna 13b Q3: https://huggingface.co/TheBloke/vicuna-13B-v1.5-16K-GGUF/blob/main/vicuna-13b-v1.5-16k.Q3_K_L.gguf
